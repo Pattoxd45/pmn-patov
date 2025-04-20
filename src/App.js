edd8b27a7a8ff20dd810e2c5e55cea4e01f04bfc
Pattoxd45 <valdespatricio832@gmail.com>
2025-04-20 15:47:19 -0400

    si

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Registro from './pages/Registro';
+import Dashboard from './pages/Dashboard';
+import PerfilUsuario from './pages/PerfilUsuario';
+import Configuracion from './pages/Configuracion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/perfil" element={<PerfilUsuario />} />
+        <Route path="/configuracion" element={<Configuracion />} />
+      </Routes>
+    </Router>
   );
 }
 
