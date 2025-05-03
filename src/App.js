198297e011d3c08bc1fef28718fcf4235b0ab9b0
Pattoxd45 <valdespatricio832@gmail.com>
2025-05-03 14:02:53 -0400

    app.js
    hacemos import al app.css para usar el  estilo y colores

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
+import './App.css';
 
 import Layout from './components/Layout';
 import Inicio from './pages/Inicio'; 
